2940785887c0ec0c509bb9df3b535106dafa29e0
nikocent <nicocenteno202@gmail.com>
2023-07-16 20:53:57 -0300

    Implemented phone update for duplicated names in backend (step 5)

--- index.js
+++ index.js
@@ -72,6 +72,23 @@ app.post('/api/persons', (req, res) => {
       })
 })
 
+app.put('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person
+    .findByIdAndUpdate(id, person, {new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
