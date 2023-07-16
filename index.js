7b5a63566882280473ba0f258e27b652c7125c1b
nikocent <nicocenteno202@gmail.com>
2023-07-15 23:42:43 -0300

    Added production build + minifixes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 app.use(express.json())
 morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 let persons = [
     { 
